e3c8027d9247ba8fefbe12664ff8d126a73be4b2
biswajit dey <bdey862002@gmail.com>
2024-05-06 12:26:04 +0530

    Q7 is Uploded

---
+++ Assignment_23/Q7.cpp
@@ -0,0 +1,17 @@
+// Write a C++ program to calculate the square of a number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,squ=0;
+	cout<<"Enter a number : ";
+	cin>>n;
+	
+	squ = n*n;
+	
+	cout<<"calculate the square of " <<n <<" = "<<squ;
+	
+	return(0);
+}
