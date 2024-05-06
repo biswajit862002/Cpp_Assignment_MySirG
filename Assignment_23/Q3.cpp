a5d6d732a95a6ae0fb3e44352614319158f9cd19
biswajit dey <bdey862002@gmail.com>
2024-05-06 12:09:59 +0530

    Q3 is Uploded

---
+++ Assignment_23/Q3.cpp
@@ -0,0 +1,18 @@
+// Write a C++ program to calculate the sum of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,sum;
+	
+	cout<<"Enter two number : ";
+	cin>>a>>b;
+	
+	sum = a+b;
+	
+	cout<<"Sum of two number is = "<<sum;
+	
+	return(0);
+}
